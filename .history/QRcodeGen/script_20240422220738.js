71073a25220ca2e689d55ef131649702cacfcfa5
thisissahaj <s21sahaj@gmail.com>
2024-04-22 22:23:37 +0530

    qrcodegen

---
+++ .history/QRcodeGen/script_20240422220738.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('#input');
+const qrBox = document.querySelector('#qrBox');
+const qrImg = document.querySelector('#qrImg');
+
+const generate = () => {
+    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+ input.value;
+
+    qrBox.classList.add('show-img')
+}
---
+++ .history/QRcodeGen/script_20240422221325.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('#input');
+const qrBox = document.querySelector('#qrBox');
+const qrImg = document.querySelector('#qrImg');
+
+const generate = () => {
+
+    if (input.value.length > 0) {
+        
+        qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='+ input.value;
+        qrBox.classList.add('show-img');
+    }
+    //to show error if there is no input
+    else{
+        input.classList.add('error');
+
+        setTimeout(() => {
+         input.classList.remove('error');
+        }, 1000);
+    }
+}
